91bc4a098d3213d2cf79e4a177888d127831cf7e
daxchx <daxchx.v1@gmail.com>
2024-05-01 15:02:18 +0900

    add: ルーティングの生成 (#2)

---
+++ src/app/focus/page.tsx
@@ -0,0 +1,7 @@
+export default function FocusPage() {
+  return (
+    <>
+      <div>focus</div>
+    </>
+  )
+}
---
+++ src/app/prepair-second/page.tsx
@@ -0,0 +1,7 @@
+export default function PrepairSecondPage() {
+  return (
+    <>
+      <h1>prepair second</h1>
+    </>
+  )
+}
