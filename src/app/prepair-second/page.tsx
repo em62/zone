93fb2e572659bbdf952a69a1bb6f11b4164ec85b
daxchx <daxchx.v1@gmail.com>
2024-05-03 12:37:32 +0900

    refactor: フォーマットの修正 (#13)

--- src/app/prepair-second/page.tsx
+++ src/app/prepair-second/page.tsx
@@ -2,11 +2,9 @@ export default function PrepairSecondPage() {
   return (
     <>
       <div className="flex min-h-screen flex-col justify-center items-center">
-        <div className="absolute top-0 mt-10 text-gray-500 text-sm">
-          本を見つめましょう
-        </div>
+        <div className="absolute top-0 mt-10 text-gray-500 text-sm">本を見つめましょう</div>
         <div className="text-4xl">🌲</div>
       </div>
     </>
-  );
+  )
 }
