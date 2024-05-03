ab799877f7248cb44992a30ab56d6115b2f16b2e
daxchx <daxchx.v1@gmail.com>
2024-05-03 12:27:30 +0900

    add: UIを初期化 (#12)

--- src/app/prepair-first/page.tsx
+++ src/app/prepair-first/page.tsx
@@ -1,7 +1,12 @@
 export default function PrepairFirstPage() {
   return (
     <>
-      <h1>prepair-first</h1>
+      <div className="flex min-h-screen flex-col justify-center items-center">
+        <div className="absolute top-0 mt-10 text-gray-500 text-sm">
+          画面の表示に合わせて深呼吸をします。
+        </div>
+        <div>息を吸って</div>
+      </div>
     </>
   );
 }
