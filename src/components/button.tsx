1692d1644f3c49f0f8defc682b24f86b0bd92826
daxchx <daxchx.v1@gmail.com>
2024-05-04 12:16:07 +0900

    feat: onClickEventを追加する (#27)

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,3 +1,12 @@
-export const Button = ({ children }: { children: React.ReactNode }) => {
-  return <button className="bg-white text-black text-sm rounded py-3 px-4 whitespace-nowrap	">{children}</button>
+type Props = {
+  children: React.ReactNode
+  onClick?: () => void
+}
+
+export const Button = ({ children, onClick }: Props) => {
+  return (
+    <button className="bg-white text-black text-sm rounded py-3 px-4 whitespace-nowrap" onClick={onClick}>
+      {children}
+    </button>
+  )
 }
