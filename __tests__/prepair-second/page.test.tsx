d35741b693058a2de1fa5622f3bed85301e63690
daxchx <daxchx.v1@gmail.com>
2024-05-04 10:56:23 +0900

    test: routerのレンダリングを追加する (#21)

--- __tests__/prepair-second/page.test.tsx
+++ __tests__/prepair-second/page.test.tsx
@@ -1,10 +1,16 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import PrepairSecondPage from "@/app/prepair-second/page";
+import { AppRouterContextProviderMock } from "../../provider/app-router-context-provider-mock";
 
 describe("PrepairSecondPageコンポーネント", () => {
   test("テキストのレンダリングを確認する", () => {
-    render(<PrepairSecondPage />);
+    const push = jest.fn();
+    render(
+      <AppRouterContextProviderMock router={{ push }}>
+        <PrepairSecondPage />
+      </AppRouterContextProviderMock>,
+    );
     const heading = screen.getByText("本を見つめましょう");
     const text = screen.getByText("🌲");
     expect(heading).toBeInTheDocument();
