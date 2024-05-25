2ab3686065b168122514d78f152f7def5b425929
daxchx <daxchx.v1@gmail.com>
2024-05-25 23:12:14 +0900

    refactor: コンポーネントを修正する (#108)

--- src/app/zone/page.tsx
+++ src/app/zone/page.tsx
@@ -1,31 +1,11 @@
-'use client'
+import { CurrentPhase } from '@/components/zone/phase'
+import { createClient } from '@/utils/supabase/server'
 
-import { useAppDispatch, useAppSelector } from '@/lib/hooks'
-import Breathe from '@/app/zone/prepair-first'
-import PrepairSecond from './prepair-second'
-import ZonePage from './zone'
-import BreakPage from './break'
-import { useEffect } from 'react'
-import { update } from '@/lib/features/currentPage/currentPageSlice'
-import End from './end'
+export default async function Zone() {
+  const db = createClient()
+  const {
+    data: { user },
+  } = await db.auth.getUser()
 
-export default function Zone() {
-  const currentPage = useAppSelector((state: any) => state.currentPage)
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    return () => {
-      dispatch(update('prepair1'))
-    }
-  }, [])
-
-  return (
-    <>
-      {currentPage == 'prepair1' && <Breathe />}
-      {currentPage == 'prepair2' && <PrepairSecond />}
-      {currentPage == 'zone' && <ZonePage />}
-      {currentPage == 'break' && <BreakPage />}
-      {currentPage == 'end' && <End />}
-    </>
-  )
+  return <CurrentPhase user={user} />
 }
