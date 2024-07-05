ec7494f9759e170822d517283d073c59f00c780c
daxchx <daxchx.v1@gmail.com>
2024-07-05 11:33:13 +0900

    move: ディレクトリを移動する (#125)

---
+++ src/db/actions.ts
@@ -0,0 +1,31 @@
+'use server'
+
+import { cache } from 'react'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/db/server'
+
+export const getUser = cache(async () => {
+  const db = createClient()
+  const {
+    data: { user },
+  } = await db.auth.getUser()
+  return user
+})
+
+export const getRecord = cache(async (id: string) => {
+  const db = createClient()
+  const { data } = await db.from('record').select().eq('user_id', id).order('created_at', { ascending: false })
+  return data
+})
+
+export const insertRecord = async (text: string) => {
+  const db = createClient()
+  await db.from('record').insert({
+    text: text,
+  })
+
+  revalidatePath('/record', 'page')
+  redirect('/')
+}
