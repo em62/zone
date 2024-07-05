00047c19225bfb2027561ff06570d4b7cb0ed879
daxchx <daxchx.v1@gmail.com>
2024-07-05 12:08:12 +0900

    feat: buttonの切り替えを削除する (#125)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,35 @@
 import { Suspense } from 'react'
-import { ButtonGroup } from '@/components/button-group'
+import Link from 'next/link'
+
+import { Account } from '@/components/account'
 import { ModeToggle } from '@/components/mode-toggle'
-import { Auth } from '@/components/auth'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between">
-      <div className="mt-4 flex h-10 w-full items-center justify-between px-4">
-        <Suspense fallback={<p>loading...</p>}>
-          <Auth />
+    <>
+      <div className="mx-auto mt-4 flex h-10 w-full max-w-2xl items-center justify-between px-4">
+        <Suspense fallback={<>loading...</>}>
+          <Account />
         </Suspense>
         <ModeToggle />
       </div>
-      <h1 className="text-4xl font-semibold">ZONE</h1>
-      <div className="mb-8 flex items-center gap-x-4">
-        <ButtonGroup />
+      <div className="px-4">
+        <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
+          <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">ZONE</h1>
+          <span className="max-w-[750px] text-center text-lg font-light text-foreground" data-br=":r2l:" data-brr="1">
+            Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.
+          </span>
+          <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-10">
+            <Button asChild size="sm">
+              <Link href="/zone">Get Started</Link>
+            </Button>
+            <Button asChild variant="outline" size="sm">
+              <Link href="/record">Record</Link>
+            </Button>
+          </div>
+        </section>
       </div>
-    </div>
+    </>
   )
 }
