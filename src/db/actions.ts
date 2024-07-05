d13e9c7eb118350ee51db293378bb8dfce86e79e
daxchx <daxchx.v1@gmail.com>
2024-07-05 12:01:36 +0900

    add: signIn関数を追加する (#125)

--- src/db/actions.ts
+++ src/db/actions.ts
@@ -2,9 +2,28 @@
 
 import { cache } from 'react'
 import { revalidatePath } from 'next/cache'
+import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 import { createClient } from '@/db/server'
+
+export const signIn = async () => {
+  const db = createClient()
+  const origin = headers().get('origin')
+
+  const { data, error } = await db.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: `${origin}/auth/callback`,
+    },
+  })
+
+  if (error) {
+    console.error(error)
+  } else {
+    return redirect(data.url)
+  }
+}
 
 export const getUser = cache(async () => {
   const db = createClient()
