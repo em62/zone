1539467c1ff26b2c21e21e9ad61453c03e3adb85
daxchx <daxchx.v1@gmail.com>
2024-07-08 23:03:31 +0900

    update: デザインを更新する (#138)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,50 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
   return (
     <>
-      <div className="px-4">
-        <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
-          <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">ZONE</h1>
-          <span className="max-w-[750px] text-center text-lg font-light text-foreground" data-br=":r2l:" data-brr="1">
-            Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.
-          </span>
-          <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-10">
-            <Button asChild size="sm">
-              <Link href="/zone">Get Started</Link>
-            </Button>
-            <Button asChild variant="outline" size="sm">
-              <Link href="/record">Record</Link>
-            </Button>
-          </div>
-        </section>
-      </div>
+      <section className="mx-auto flex flex-col items-start gap-2 px-4 py-8 md:py-12 md:pb-8 lg:py-12 lg:pb-10">
+        <a className="group inline-flex items-center px-0.5 text-sm font-medium" href="/docs/components/chart">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-pie-chart h-4 w-4"
+          >
+            <path d="M21.21 15.89A10 10 0 1 1 8 2.83"></path>
+            <path d="M22 12A10 10 0 0 0 12 2v10z"></path>
+          </svg>{' '}
+          <div data-orientation="vertical" role="none" className="mx-2 h-4 w-[1px] shrink-0 bg-border"></div> <span className="underline-offset-4 group-hover:underline">User Guide</span>
+          <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="ml-1 h-4 w-4">
+            <path
+              d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+              fill="currentColor"
+              fillRule="evenodd"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </a>
+        <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1]">The best learning environment</h1>
+        <p className="max-w-2xl text-lg font-light text-foreground">Creating an environment is important for concentrating on learning. </p>
+        <div className="flex w-full items-center justify-start gap-2 py-2">
+          <Link
+            className="inline-flex h-8 items-center justify-center whitespace-nowrap rounded-md bg-primary px-3 text-xs font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+            href="/zone"
+          >
+            Get Started
+          </Link>
+          <Link
+            className="inline-flex h-8 items-center justify-center whitespace-nowrap rounded-md px-3 text-xs font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+            href="/record"
+          >
+            Record
+          </Link>
+        </div>
+      </section>
     </>
   )
 }
