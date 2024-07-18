8b79b7f5d94d15ca793096a558185d2f2b936abc
daxchx <daxchx.v1@gmail.com>
2024-07-18 18:04:39 +0900

    refactor: リファクタリングする

--- src/app/record/page.tsx
+++ src/app/record/page.tsx
@@ -3,25 +3,21 @@ import Link from 'next/link'
 
 import { ArrowLeft } from 'lucide-react'
 
-import { getRecord, getUser } from '@/db/actions'
+import { getRecords, getUser } from '@/db/queries'
 import { Record } from '@/type/record'
-
-export const revalidate = 3600
 
 export default function RecordPage() {
   return (
-    <>
-      <section className="mx-auto flex flex-col items-start gap-2 px-4 py-8 md:py-12 md:pb-8 lg:py-12 lg:pb-10">
-        <Link href="/" className="flex items-center gap-x-1 text-sm">
-          <ArrowLeft className="h-4 w-4" />
-          Back
-        </Link>
-        <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1]">Record</h1>
-        <Suspense fallback={<p>loading...</p>}>
-          <Records />
-        </Suspense>
-      </section>
-    </>
+    <section className="mx-auto flex flex-col items-start gap-2 px-4 py-8 md:py-12 md:pb-8 lg:py-12 lg:pb-10">
+      <Link href="/" className="flex items-center gap-x-1 text-sm">
+        <ArrowLeft className="h-4 w-4" />
+        Back
+      </Link>
+      <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1]">Record</h1>
+      <Suspense fallback={<p>loading...</p>}>
+        <Records />
+      </Suspense>
+    </section>
   )
 }
 
@@ -32,7 +28,7 @@ async function Records() {
     return <p className="leading-7 [&:not(:first-child)]:mt-6">データを表示するにはログインしてください</p>
   }
 
-  const records = await getRecord(user?.id)
+  const records = await getRecords(user?.id)
 
   if (records?.length == 0) {
     return <p className="leading-7 [&:not(:first-child)]:mt-6">データがありません。</p>
