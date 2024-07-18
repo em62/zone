1696260855afd5c8d145159da1cbcac9ca329d7a
daxchx <daxchx.v1@gmail.com>
2024-07-18 18:09:27 +0900

    fix: getUser()の呼び出しを変更する (#169)

--- src/components/zone/phase.tsx
+++ src/components/zone/phase.tsx
@@ -1,17 +1,29 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import BreakPage from '@/components/zone/break'
 import Breathe from '@/components/zone/deepbreath'
 import End from '@/components/zone/end'
 import PrepairSecond from '@/components/zone/prepair-second'
 import ZonePage from '@/components/zone/zone'
+import { getUser } from '@/db/queries'
+import { User } from '@supabase/supabase-js'
 
 const pages = ['prepair1', 'prepair2', 'zone', 'break', 'end']
 
 export const CurrentPhase = () => {
   const [current, setCurrent] = useState(pages[0])
+  const [user, setUser] = useState<User | null>(null)
+
+  useEffect(() => {
+    const initialize = async () => {
+      const user = await getUser()
+      setUser(user)
+    }
+
+    initialize()
+  }, [])
 
   return (
     <>
@@ -24,7 +36,7 @@ export const CurrentPhase = () => {
       ) : current == pages[3] ? (
         <BreakPage setCurrent={setCurrent} />
       ) : (
-        <End />
+        <End user={user} />
       )}
     </>
   )
