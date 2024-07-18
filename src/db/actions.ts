f11f276ba874e9359311bedbe548d74cedc4fb07
daxchx <daxchx.v1@gmail.com>
2024-07-18 17:59:01 +0900

    remove: 不要な関数を削除する (#169)

--- src/db/actions.ts
+++ src/db/actions.ts
@@ -1,7 +1,5 @@
 'use server'
 
-import { cache } from 'react'
-import { revalidatePath, unstable_cache } from 'next/cache'
 import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
 
@@ -23,29 +21,4 @@ export const signIn = async () => {
   } else {
     return redirect(data.url)
   }
-}
-
-export const getUser = cache(async () => {
-  const db = createClient()
-  const {
-    data: { user },
-  } = await db.auth.getUser()
-  return user
-})
-
-export const getRecord = cache(async (id: string) => {
-  const db = createClient()
-  const { data } = await db.from('records').select().eq('user_id', id).order('created_at', { ascending: false })
-  return data
-})
-
-export const insertRecord = async (text: string) => {
-  const db = createClient()
-  const { error } = await db.from('records').insert({
-    text: text,
-  })
-
-  revalidatePath('/record', 'page')
-
-  return error
 }
