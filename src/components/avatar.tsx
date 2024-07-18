b1e48e0c5d763216720d54a69f7eea61de85ad82
daxchx <daxchx.v1@gmail.com>
2024-07-18 18:02:43 +0900

    refactor: リファクタリングする

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,24 +1,20 @@
 import Image from 'next/image'
 
-import { Button } from '@/components/ui/button'
-import { getUser } from '@/db/actions'
-import { CircleUser } from 'lucide-react'
 import { PersonIcon } from '@radix-ui/react-icons'
+
+import { Button } from '@/components/ui/button'
+import { getUser } from '@/db/queries'
 
 export async function Avatar() {
   const user = await getUser()
 
-  return (
-    <>
-      {user ? (
-        <Button className="h-8 w-8 rounded-full" variant="ghost" size="icon" asChild>
-          <Image src={user.user_metadata.avatar_url} alt="" width={32} height={32} />
-        </Button>
-      ) : (
-        <Button className="px h-8 w-8 rounded-full p-2" variant="secondary" size="icon" asChild>
-          <PersonIcon />
-        </Button>
-      )}
-    </>
+  return user ? (
+    <Button className="h-8 w-8 rounded-full" variant="ghost" size="icon" asChild>
+      <Image src={user.user_metadata.avatar_url} alt="" width={32} height={32} />
+    </Button>
+  ) : (
+    <Button className="px h-8 w-8 rounded-full p-2" variant="secondary" size="icon" asChild>
+      <PersonIcon />
+    </Button>
   )
 }
